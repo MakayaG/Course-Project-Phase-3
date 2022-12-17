3dfc951d65431128d05004af352ddce1ec42a587
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-12-17 12:50:25 -0800

    Add project files.

---
+++ Course_Project_Phase_3.py
@@ -0,0 +1,7 @@
+#   Makaya Griffin
+#   CIS261
+#   Project Phase 3
+from datetime import datetime   ###################################################
+
+def GetEmpName():
+    empname = input("Enter employee name: ")
